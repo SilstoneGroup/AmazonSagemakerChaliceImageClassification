5dfcada470b91036b9c59a1009a2e9efb287b9ce
Silstone <60641991+SilstoneGroup@users.noreply.github.com>
2020-02-05 16:03:31 +0530

    Update app.py

--- Amazon Sagemaker, Chalice Code/app.py
+++ Amazon Sagemaker, Chalice Code/app.py
@@ -1,14 +1,14 @@
 from chalice import Chalice
 from chalice import BadRequestError
 import boto3, json
-# import os
-# import random
-# from scipy import ndarray
-# import skimage as sk
-# from skimage import transform
-# from skimage import util
-# from skimage import io
-# import base64
+import os
+import random
+from scipy import ndarray
+import skimage as sk
+from skimage import transform
+from skimage import util
+from skimage import io
+import base64
 
 
 app = Chalice(app_name='Tuwi')
@@ -54,7 +54,7 @@ def train_job_by_name():
     if 'resize' in body:
         job['HyperParameters']['resize'] = body['resize']
 
-    # return {'ResponseMetadata': str(job)}
+    return {'ResponseMetadata': str(job)}
     if 'VpcConfig' in job:
         resp = sm.create_training_job(
             TrainingJobName=job['TrainingJobName'], AlgorithmSpecification=job['AlgorithmSpecification'], RoleArn=body['RoleArn'],
@@ -64,7 +64,7 @@ def train_job_by_name():
             VpcConfig=job['VpcConfig'],
             Tags=job['Tags'] if 'Tags' in job else [])
     else:
-        # Because VpcConfig cannot be empty like HyperParameters or Tags :-/
+        Because VpcConfig cannot be empty like HyperParameters or Tags :-/
         resp = sm.create_training_job(
             TrainingJobName=job['TrainingJobName'], AlgorithmSpecification=job['AlgorithmSpecification'], RoleArn=body['RoleArn'],
             InputDataConfig=job['InputDataConfig'], OutputDataConfig=job['OutputDataConfig'],
@@ -116,75 +116,75 @@ def create_model(modelname, trainingjobname):
     status = response['EndpointStatus']
     return {'Endpoint Created, Status:' + status}
 
-    # wait until the status has changed
-    # sm.get_waiter('endpoint_in_service').wait(EndpointName=endpoint_name)
-    #
-    # # print the status of the endpoint
-    # endpoint_response = sm.describe_endpoint(EndpointName=endpoint_name)
-    # status = endpoint_response['EndpointStatus']
-    # return {'Endpoint Created'}
-    #
-    # if status != 'InService':
-    #     return {'Endpoint creation failed'}
+    wait until the status has changed
+    sm.get_waiter('endpoint_in_service').wait(EndpointName=endpoint_name)
+    
+    # print the status of the endpoint
+    endpoint_response = sm.describe_endpoint(EndpointName=endpoint_name)
+    status = endpoint_response['EndpointStatus']
+    return {'Endpoint Created'}
+    
+    if status != 'InService':
+        return {'Endpoint creation failed'}
 
 
 
 
 
-# @app.route('/image', methods=['POST'])
-# def imagetry():
-#     # dictionary of the transformations we defined earlier
-#     available_transformations = {
-#         'rotate': random_rotation,
-#         'noise': random_noise,
-#         'horizontal_flip': horizontal_flip
-#     }
-#     folder_path = 'images/cat'
-#     num_files_desired = 10
-#     # find all files paths from the folder
-#     images = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
-#     # return {'ResponseMetadata': str(images)}
-#     num_generated_files = 0
-#     while num_generated_files <= num_files_desired:
-#         # random image from the folder
-#         image_path = random.choice(images)
-#         # read image as an two dimensional array of pixels
-#         image_to_transform = sk.io.imread(image_path)
-#         # random num of transformation to apply
-#         num_transformations_to_apply = random.randint(1, len(available_transformations))
-#
-#         num_transformations = 0
-#         transformed_image = None
-#         while num_transformations <= num_transformations_to_apply:
-#
-#             # random transformation to apply for a single image
-#             key = random.choice(list(available_transformations))
-#             transformed_image = available_transformations[key](image_to_transform)
-#             num_transformations += 1
-#             new_file_path = '%s/aug_image_%s.jpg' % (folder_path, num_generated_files)
-#             return {'ResponseMetadata': "uploaded"}
-#             # uploadOnS3(transformed_image)
-#             # write image to the disk
-#             io.imsave(new_file_path, transformed_image)
-#         num_generated_files += 1
-#
-#
-# def uploadOnS3():
-#     uploadedfile = s3.meta.client.upload_file(Filename='air.jpg', Bucket='testdatauploads', Key='testimage.png')
-#     return uploadedfile
-#
-#
-# def random_rotation(image_array: ndarray):
-#     # pick a random degree of rotation between 25% on the left and 25% on the right
-#     random_degree = random.uniform(-25, 25)
-#     return sk.transform.rotate(image_array, random_degree)
-#
-#
-# def random_noise(image_array: ndarray):
-#     # add random noise to the image
-#     return sk.util.random_noise(image_array)
-#
-#
-# def horizontal_flip(image_array: ndarray):
-#     # horizontal flip doesn't need skimage, it's easy as flipping the image array of pixels !
-#     return image_array[:, ::-1]
+@app.route('/image', methods=['POST'])
+def imagetry():
+    # dictionary of the transformations we defined earlier
+    available_transformations = {
+        'rotate': random_rotation,
+        'noise': random_noise,
+        'horizontal_flip': horizontal_flip
+    }
+    folder_path = 'images/cat'
+    num_files_desired = 10
+    # find all files paths from the folder
+    images = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if os.path.isfile(os.path.join(folder_path, f))]
+    # return {'ResponseMetadata': str(images)}
+    num_generated_files = 0
+    while num_generated_files <= num_files_desired:
+        # random image from the folder
+        image_path = random.choice(images)
+        # read image as an two dimensional array of pixels
+        image_to_transform = sk.io.imread(image_path)
+        # random num of transformation to apply
+        num_transformations_to_apply = random.randint(1, len(available_transformations))
+
+        num_transformations = 0
+        transformed_image = None
+        while num_transformations <= num_transformations_to_apply:
+
+            # random transformation to apply for a single image
+            key = random.choice(list(available_transformations))
+            transformed_image = available_transformations[key](image_to_transform)
+            num_transformations += 1
+            new_file_path = '%s/aug_image_%s.jpg' % (folder_path, num_generated_files)
+            return {'ResponseMetadata': "uploaded"}
+            # uploadOnS3(transformed_image)
+            # write image to the disk
+            io.imsave(new_file_path, transformed_image)
+        num_generated_files += 1
+
+
+def uploadOnS3():
+    uploadedfile = s3.meta.client.upload_file(Filename='air.jpg', Bucket='testdatauploads', Key='testimage.png')
+    return uploadedfile
+
+
+def random_rotation(image_array: ndarray):
+    # pick a random degree of rotation between 25% on the left and 25% on the right
+    random_degree = random.uniform(-25, 25)
+    return sk.transform.rotate(image_array, random_degree)
+
+
+def random_noise(image_array: ndarray):
+    # add random noise to the image
+    return sk.util.random_noise(image_array)
+
+
+def horizontal_flip(image_array: ndarray):
+    # horizontal flip doesn't need skimage, it's easy as flipping the image array of pixels !
+    return image_array[:, ::-1]
